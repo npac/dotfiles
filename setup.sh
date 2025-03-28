9233a4834262d0483898c26bc49f19e20c4909dc
Pavel Capcanari <pavel.capcanari.ext@nokia.com>
2025-03-28 10:19:35 -0700

    get away from nvim

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #tmux
-git -C $HOME clone https://github.com/jimeh/tmux-themepack.git
+git -C $HOME clone https://github.com/jimeh/tmux-themepack.git .tmux-themepack
 ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
 
-# Plug (to be used with nvim)
-# https://github.com/junegunn/vim-plug
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# # Plug (to be used with nvim)
+# # https://github.com/junegunn/vim-plug
+# curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# nvim
-mkdir -p ~/.config/nvim
-ln -sf `pwd`/nvim/init.vim ~/.config/nvim/
-#start nvim and trigger plugin install, exit after installation
-nvim +PlugInstall +qa
+# # nvim
+# mkdir -p ~/.config/nvim
+# ln -sf `pwd`/nvim/init.vim ~/.config/nvim/
+# #start nvim and trigger plugin install, exit after installation
+# nvim +PlugInstall +qa
 
-echo 'NeoVim configuration completed. Enjoy!'
+# echo 'NeoVim configuration completed. Enjoy!'
