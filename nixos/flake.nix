0b0c77848753434ad7f039d595a08762d28ebb4f
npac <pcapcanari@gmail.com>
2026-01-08 13:46:05 -0800

    import wsl

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,16 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, nixos-wsl, ... }: 
     let
        lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
             system = "x86_64-linux";
-            modules = [ ./configuration.nix ];
+            modules = [ 
+              nixos-wsl.nixosModules.default
+              ./configuration.nix 
+            ];
         };
       };
     };
