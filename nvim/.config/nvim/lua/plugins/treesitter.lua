92c4dcd0971a8ac6869037a693b73484b6001d6c
npac <pcapcanari@gmail.com>
2026-01-23 11:12:46 -0800

    working ascciidoc treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,52 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    enabled = true,
-    lazy = false,
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs", -- redirect setup to correct module
-    opts = function(_, _)
-      return {
-        ensure_installed = {
-          "lua",
-          "tsx",
-          "typescript",
-          "go",
-          "gomod",
-          "gosum",
-          "yaml",
-          "regex",
-          "html",
-          "css",
-          "bash",
-          "javascript",
-          "latex",
-          "norg",
-          "scss",
-          "svelte",
-          "typst",
-          "vue",
-          "nix",
-        },
-        highlight = { enable = true },
-        indent = { enabled = true },
-      }
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      if not vim.tbl_contains(opts.ensure_installed, "asciidoc") then
+        table.insert(opts.ensure_installed, "asciidoc")
+      end
+    end,
+
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          require("nvim-treesitter.parsers").asciidoc = {
+            install_info = {
+              location = "tree-sitter-asciidoc",
+              revision = "276ae6b766830e9d9de9106713d94a6913279bc2",
+              url = "https://github.com/cathaysia/tree-sitter-asciidoc",
+              branch = "master",
+              generate = false,
+              generate_from_json = false,
+              queries = "tree-sitter-asciidoc/queries",
+            },
+            requires = { "asciidoc_inline" },
+            tier = 2,
+          }
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          require("nvim-treesitter.parsers").asciidoc_inline = {
+            install_info = {
+              location = "tree-sitter-asciidoc_inline",
+              revision = "276ae6b766830e9d9de9106713d94a6913279bc2",
+              url = "https://github.com/cathaysia/tree-sitter-asciidoc",
+              branch = "master",
+              generate = false,
+              generate_from_json = false,
+              queries = "tree-sitter-asciidoc_inline/queries",
+            },
+            tier = 2,
+          }
+        end,
+      })
+
+      vim.treesitter.language.register("asciidoc", { "asciidoc", "adoc" })
     end,
   },
 }
